eb7433ca97edfc8be8c66cf4b65ef4be79b7a31a
codesharedot <54853330+codesharedot@users.noreply.github.com>
2019-09-14 19:54:47 +0200

    Update example.py
    fix

--- example.py
+++ example.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import requests
 import json
 from forex_python.converter import CurrencyRates
@@ -7,8 +8,8 @@ c = CurrencyRates()
 rate = c.get_rate('USD', 'EUR') 
 print(rate)
 
-basic-attention-token_api_url = 'https://api.coinmarketcap.com/v1/ticker/basic-attention-token/'
-response = requests.get(basic-attention-token_api_url)
+basic_attention_token_api_url = 'https://api.coinmarketcap.com/v1/ticker/basic-attention-token/'
+response = requests.get(basic_attention_token_api_url)
 response_json = response.json()
 print(response_json)
 
@@ -17,4 +18,4 @@ for coin in response.json():
     coin_price = float(("{0:.2f}").format(float(price)))
     print("$ " + str(coin_price))
     coin_price_eur = float(("{0:.2f}").format(float(price)*rate))   
-    print("€ " + str(coin_price_eur))
+    print("E " + str(coin_price_eur))
